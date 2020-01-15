1720acd0efcd9d1b397b27f691ce6fbcebbccb1c
Rubén Béjar <rbejar@unizar.es>
2020-01-15 16:20:51 +0100

    Deployment requires a GitHub Token

--- build-scripts/deploy.sh
+++ build-scripts/deploy.sh
@@ -3,6 +3,9 @@ echo
 echo "Publishing package script ... "
 echo
 
+# Permission to publish to Github package repository is required
+npm set //npm.pkg.github.com/:_authToken $GITHUB_API_KEY
+
 cd $TRAVIS_BUILD_DIR
 if ./gradlew npmPublish; then    
     echo
