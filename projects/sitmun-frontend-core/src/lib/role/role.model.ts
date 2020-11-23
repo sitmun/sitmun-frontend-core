35afefe68a448b77c76edb9e62727f8b7c085088
gon-nexus <ggaliano@nexusgeographics.com>
2020-11-23 08:48:32 +0100

    Añadida base nexus

--- projects/sitmun-frontend-core/src/lib/role/role.model.ts
+++ projects/sitmun-frontend-core/src/lib/role/role.model.ts
@@ -4,9 +4,11 @@ import {Resource} from '../angular-hal/src/lib/resource';
  * Role model
  */
 export class Role extends Resource {
+  /** id */
+  public id: number;
   /** name*/
   public name: string;
   /** comments*/
-  public comments: string;
+  public description: string;
 
 }
--- projects/sitmun-frontend-core/src/lib/territory/territory-type.model.ts
+++ projects/sitmun-frontend-core/src/lib/territory/territory-type.model.ts
@@ -4,6 +4,8 @@ import {Resource} from '../angular-hal/src/lib/resource';
  * Territory type model
  */
 export class TerritoryType extends Resource {
+   /** id */
+   public id: number;  
   /** name */
   public name: string;
 }
