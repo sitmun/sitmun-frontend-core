58f44429289676f5dd311b3a5ab08d164bbe2cde
JLucena <jlucena@nexusgeographics.com>
2021-02-01 15:35:01 +0100

    fix import rjxs bug

--- projects/sitmun-frontend-core/src/lib/cartography/cartography.service.ts
+++ projects/sitmun-frontend-core/src/lib/cartography/cartography.service.ts
@@ -1,7 +1,7 @@
 import { Cartography } from './cartography.model';
 import { Injectable, Injector } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { RestService } from '../angular-hal/src/lib/rest.service';
 
 /** Cartography manager service */
